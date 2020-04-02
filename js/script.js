21a1aa2881078f3838796ac11ce76ef44bc4834f
Andre <mae.26@yandex.ru>
2020-04-02 22:50:13 +0700

    colors

--- js/script.js
+++ js/script.js
@@ -35,15 +35,21 @@ const yAxis = svg.append('g').attr('transform', `translate(60, 0)`)//.call(y_axi
 
 
 // Part 2: Здесь можно создать шкалы для цвета и радиуса объектов
-// const color = d3.scaleOrdinal()...
-// const r = d3.scaleSqrt()...
+const color = d3.scaleOrdinal().range(colors)
+const r = d3.scaleSqrt().range([5, 50])
 
 // Part 2: для элемента select надо задать options http://htmlbook.ru/html/select
 // и установить selected для дефолтного значения
 
 // d3.select('#radius').selectAll('option')
 //         ...
-
+selector=document.getElementById('radius')
+for (i in params){
+  var option = document.createElement("option");
+  option.text = params[i];
+  selector.add(option,selector[0]);
+};
+d3.select('#radius').selectAll('option')
 
 // Part 3: то же что делали выше, но для осей
 // ...
@@ -51,7 +57,8 @@ const yAxis = svg.append('g').attr('transform', `translate(60, 0)`)//.call(y_axi
 
 loadData().then(data => {
     // сюда мы попадаем после загружки данных и можем для начала на них посмортеть:
-    //console.log(data.map(a => a.population))
+    console.log(data.map(d=> d['region']));
+    console.log(d3.nest().key(function(d,i){return d['region']}).entries(data).map(d=>d.key));
     //console.log(x(data.map(d=> +d[xParam][year]) ))
     //console.log(data.map(d=> +d[yParam][year]));
     //console.log(data[0]['child-mortality'])
@@ -65,8 +72,8 @@ loadData().then(data => {
 
     // Part 2: здесь мы можем задать пораметр 'domain' для цветовой шкалы
     // для этого нам нужно получить все уникальные значения поля 'region', сделать это можно при помощи 'd3.nest'
-    //let regions = d3.nest()...
-    //color.domain(regions);
+    let regions = d3.nest().key(function(d,i){return d['region']}).entries(data).map(d=>d.key)
+    color.domain(regions);
 
     // подписка на изменение позиции ползунка
     d3.select('.slider').on('change', newYear);
@@ -84,6 +91,8 @@ loadData().then(data => {
     }
 
     function newRadius(){
+      radius = this.value;
+      updateChart()
         // Part 2: по аналогии с newYear
     }
     function updateChart(){
@@ -103,10 +112,19 @@ loadData().then(data => {
         yAxis.call(d3.axisLeft(y));
         // Part 1: нужно сделать то же самое и для 'y'
         // ...
-        console.log(xRange.values());
+
+
+        //v//ar option = document.createElement("option")
+        //option.text = params[1]
+        //selector.add(option,selector[0])
+        console.log(radius);
+        console.log(color);
         // Part 2: теперь у нас есть еще одна не постоянная шкала
         // ...
-        //console.log(data)
+        let zRange = data.map(d=> +d[radius][year]);
+        r.domain([d3.min(zRange), d3.max(zRange)])
+        //y.domain([d3.min(yRange), d3.max(yRange)]);
+
         // Part 1, 2: создаем и обновляем состояние точек
         //svg.append('g').selectAll('circle').data(data).enter().append('circle')
         //.attr("cx", function (d) { return (d.population); } )
@@ -116,12 +134,15 @@ loadData().then(data => {
         var p=svg.selectAll('circle').data(data)
         .attr('cx', function(d,i) {return x(xRange[i]);})
     .attr('cy', function(d,i) {return y(yRange[i]);})
-    .attr('r', function(d) { return 10; });
+    .attr('r', function(d,i) { return r(zRange[i]); })
+    .style("fill", function(d,i) { return color(data.map(d=> d['region'])[i]);})
+    ;
 
     p.enter().append('circle')
     .attr('cx', function(d,i) {return x(xRange[i]);})
 .attr('cy', function(d,i) {return y(yRange[i]);})
-.attr('r', function(d) { return 10; });
+.attr('r', function(d,i) { return r(zRange[i]) ; })
+.style("fill", function(d,i) { return color(data.map(d=> d['region'])[i]);});
 p.exit().remove();
     }
 
